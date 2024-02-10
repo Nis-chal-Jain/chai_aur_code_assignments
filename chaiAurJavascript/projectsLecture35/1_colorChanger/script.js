dfe422939e14f343bc8dbd20d5a246ffdd26a5f8
Nischal Jain <89198650+Nis-chal-Jain@users.noreply.github.com>
2024-02-10 18:44:16 +0000

    improved project 2

--- chaiAurJavascript/projectsLecture35/1_colorChanger/script.js
+++ chaiAurJavascript/projectsLecture35/1_colorChanger/script.js
@@ -4,3 +4,17 @@ button.forEach((value)=>{
       document.body.style.backgroundColor=`${e.target.id}`
   })
 })
+
+function createNewButton(id){
+  const newbutton = document.createElement('span')
+  newbutton.className = "button"
+  newbutton.id = `${id}`
+  newbutton.style.backgroundColor=`${id}`
+  document.querySelector(".subCanvas").appendChild(newbutton)
+}
+
+const sub = document.querySelector("#createbu")
+sub.addEventListener('click',()=>{
+  const newid = document.querySelector('#create').value
+  createNewButton(newid)
+})
